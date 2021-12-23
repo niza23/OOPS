88177c845f855afcb20300956d001e0940d29909
niza23 <79857995+niza23@users.noreply.github.com>
2021-12-23 12:34:01 +0530

    Add files via upload

---
+++ Defaultconstrcutor.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+#include<string>
+using namespace std;
+class person{
+    private:
+    int age;
+    string name;
+    float height;
+
+
+    public:
+    same name as class and no return type
+    person() //no parameter
+    { //default values
+        age=0; 
+        name="Null";
+        height=0.0f;
+    }
+    // if no default constructor is defined by the user, compiler add the default constrcutor by its own.. this default constrcutor just allocate the memory and will have the garbage values
+    void get()
+    {
+        cout<<"name is:"<<name<<endl;
+        cout<<"age is:"<<age<<endl;
+        cout<<"height is"<<height<<endl;
+    }
+    
+    
+
+};
+int main()
+{
+    
+    person obj;
+    obj.get();
+   
+    return 0;
+}
