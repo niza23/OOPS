a92e9984792ed69a623a1ff5eb3cbdca508a4170
niza23 <79857995+niza23@users.noreply.github.com>
2021-12-23 12:32:34 +0530

    Add files via upload

---
+++ CLASS.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<string>
+using namespace std;
+//class
+class house{
+    //access specifier
+
+private:
+//member variable(will be hidden)
+int length=0,breadth=0;
+
+public:
+
+//member functions
+void setdata(int x,int y){
+    length=x;
+    breadth=y;
+}
+void area()
+{
+    cout<<"area is:"<<length*breadth;
+}
+};
+int main()
+{
+    //create object
+    house a;
+    a.setdata(300,500);
+    a.area();
+
+}
