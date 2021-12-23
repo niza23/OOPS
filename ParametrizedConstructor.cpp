745b5d48c47d80a93afd2d125594f850aabf3deb
niza23 <79857995+niza23@users.noreply.github.com>
2021-12-23 12:45:32 +0530

    Add files via upload

---
+++ ParametrizedConstructor.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+class person{
+    private:
+    int age;
+    string name;
+    float height;
+
+
+    public:
+    
+    person(string x,int y,int z) 
+    { 
+        name=x;
+        age=y; 
+        height=z;
+    }
+    void get()
+    {
+        cout<<"name is:"<<name<<endl;
+        cout<<"age is:"<<age<<endl;
+        cout<<"height is"<<height<<endl;
+    }
+    
+    
+
+};
+int main()
+{
+    
+    person obj("nidhi",7,6);
+    obj.get();
+   
+    return 0;
+}
