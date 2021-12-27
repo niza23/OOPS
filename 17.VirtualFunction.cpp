cf46dca70da9b5a3dd669a8af017d17d5d299865
niza23 <79857995+niza23@users.noreply.github.com>
2021-12-27 15:31:45 +0530

    Add files via upload

---
+++ 17.VirtualFunction.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+#include<string>
+using namespace std;
+class base
+{
+public:
+        int z;
+        virtual void x(){   //so that only derived class functionality gets called
+            cout<<"base class"<<endl;
+        }
+        
+
+};
+class der1: public base
+{
+public:
+        void x(){
+            cout<<"x of derived 1"<<endl;
+        }
+        
+};
+class der2: public base
+{
+public:
+        void x(){
+            cout<<"x of derived 2"<<endl;
+        }
+        
+
+};
+
+int main(){
+    der1 obj;
+    der2 obj2;
+    
+    base *ptr;
+    ptr=&obj;
+    ptr->x(); //if there is no virtual it will call the base class
+
+    ptr=&obj2;
+    ptr->x(); //if no virtual it will call the base class
+     return 0;
+}
+
+//the programe that appears to be calling a function of one class but in reality be calling function of another class
