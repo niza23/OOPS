9b59f5fa4d3959f2b7431ad644ca56e2603f37c6
niza23 <79857995+niza23@users.noreply.github.com>
2021-12-27 15:26:50 +0530

    Add files via upload

---
+++ 13.ConstructorAndInheritance3.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+#include<string>
+using namespace std;
+class A{
+    public:
+    A()
+    {
+        cout<<"default constructor of base class"<<endl;
+    }
+    A(int b)
+    {
+        cout<<"parametrized constructor of base class and the parameter is:"<<b<<endl;
+    }
+
+};
+class B : public A
+{
+    public:
+    B():A() 
+    {
+        cout<<"default of derived class in base ";
+    }
+    // compiler will first call base class
+    // then constructor of derived class
+    B(int x):A( x)
+    {
+        cout<<"para of derived class:"<<x+3;
+    }
+    // compiler will first call base class para constructor
+    // then para constructor of derived class
+
+
+};
+int main()
+{
+    B obj();
+    B obj2(3);
+    return 0;
+}
