4965d83c2900892b65a998b05be9772e325e57e8
niza23 <79857995+niza23@users.noreply.github.com>
2021-12-27 15:28:28 +0530

    Add files via upload

---
+++ 16.Polymorphism.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<string>
+using namespace std;
+class basiccar{
+    public:
+    void body(){
+        cout<<"body of basic car"<<endl;
+    }
+
+};
+class advcar: public basiccar{
+    public:
+    void engine(){
+        cout<<"this is the engine of adv car";
+    }
+
+};
+int main()
+{
+    basiccar *ptr;
+    ptr=new advcar();
+    ptr -> body();
+    // ptr -> engine(); ----this will give an error bcos 
+    return 0;
+}
