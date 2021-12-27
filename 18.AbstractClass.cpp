a25a23b81460ab898801e9791ee7e0c0c69a13d0
niza23 <79857995+niza23@users.noreply.github.com>
2021-12-27 15:32:00 +0530

    Add files via upload

---
+++ 18.AbstractClass.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+#include<string>
+using namespace std;
+class base  //abstract class
+{
+public:
+        int z;
+        virtual void x() = 0; //this is pure abstract class which exist only to act as a parent of derived class
+        // does not contain any body
+
+};
+class der1: public base
+{
+public:
+        void x(){
+            cout<<"x of derived 1"<<endl;
+        }
+        
+};
+class der2: public base
+{
+public:
+        void x(){
+            cout<<"x of derived 2"<<endl;
+        }
+        
+
+};
+
+int main(){
+    der1 obj;
+    der2 obj2;
+    
+    base *ptr;
+    ptr=&obj;
+    ptr->x(); 
+
+    ptr=&obj2;
+    ptr->x(); 
+     return 0;
+}
