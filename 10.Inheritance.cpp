f2aa4b9dc4cc226d05d7bd48428080e0f3bf3bfd
niza23 <79857995+niza23@users.noreply.github.com>
2021-12-27 15:24:38 +0530

    Add files via upload

---
+++ 10.Inheritance.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<string>
+using namespace std;
+class rec{
+    public:
+    int l,b;
+    void area()
+    {
+        cout<<"area is"<<l*b<<endl;
+    }
+
+};
+class cuboid : public rec
+{
+public:
+int h;
+void vol()
+{
+    cout<<"the volume of cuboid"<<l*b*h;
+
+}
+};
+
+int main()
+{
+    cuboid c;
+    c.l=10;
+    c.b=20;
+    c.h=30;
+    c.area();
+    c.vol();
+    return 0;
+
+}
+
+
