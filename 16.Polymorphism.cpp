679f9e95e2e9bf13fe156cacb29b5736447ce34e
Nidhi Zala <nidhizala2307@gmail.com>
2022-03-04 00:17:22 +0530

    Update 16.Polymorphism.cpp

--- 16.Polymorphism.cpp
+++ 16.Polymorphism.cpp
@@ -20,6 +20,6 @@ int main()
     basiccar *ptr;
     ptr=new advcar();
     ptr -> body();
-    // ptr -> engine(); ----this will give an error bcos 
+    // ptr -> engine(); -->>this will give an error bcos 
     return 0;
 }
